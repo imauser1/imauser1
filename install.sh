0de11857a4b24efe7528cff3c3fb299896eebd0f
imauser1 <167974453+imauser1@users.noreply.github.com>
2024-05-17 10:15:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install tigervnc-standalone-server novnc lxde firefox
+cd /
+sudo git clone https://github.com/novnc/noVNC
+cd
+wget https://raw.githubusercontent.com/imauser1/freelinux/main/start.sh
+chmod +x start.sh
+./start.sh
